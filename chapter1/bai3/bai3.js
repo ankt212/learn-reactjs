7b628b2828a59774c9ae6ff1a77ca02d9b95e5f8
rogerkieu <kieuthaian0212@gmail.com>
2022-08-20 16:30:28 +0700

    update chapter1-bai3

---
+++ chapter1/bai3/bai3.js
@@ -0,0 +1,20 @@
+// let person = "Peter";
+// let person = {
+//   firstName: "Peter",
+//   lastName: "An",
+//   age: "23",
+//   eyeColor: "black",
+// };
+const person = ["Eric", "HoiDanIT", 26];
+console.log(person);
+// console.log("Check last name: ", person.lastName);
+// console.log("Check first name: ", person["firstName"]);
+console.log("Check first name: ", person[0]);
+
+let React = {
+  language: "Javascipt",
+  author: "facebook",
+};
+console.log(React);
+console.log("Ngôn ngữ lập trình tạo ra React là: ", React.language);
+console.log("Tác giả React là: ", React["author"]);
