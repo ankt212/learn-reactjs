b9d5ced4dac3aabe7f81e48e037abd8cdb788863
rogerkieu <kieuthaian0212@gmail.com>
2022-08-22 09:27:59 +0700

    video 17

--- demo/src/components/MyComponent.js
+++ demo/src/components/MyComponent.js
@@ -4,8 +4,17 @@ import React from "react";
 
 class MyComponent extends React.Component {
   //jsx
+  state = {
+    name: "An Peter",
+    address: "Hà Đông",
+    age: 23,
+  };
   render() {
-    return <div>My first component</div>;
+    return (
+      <div>
+        My name is {this.state.name} and I'm form {this.state.address}
+      </div>
+    );
   }
 }
 export default MyComponent;
