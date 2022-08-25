3a0b77e4c24a37059169a2a8de8498acf0c917a7
rogerkieu <kieuthaian0212@gmail.com>
2022-08-25 16:46:44 +0700

    design homepage

--- demo/src/components/Header/Header.js
+++ demo/src/components/Header/Header.js
@@ -27,12 +27,14 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+            <button className="btn-login">Login</button>
+            <button className="btn-signup">Sign up</button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item>Login</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item>Log out</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- demo/src/components/Home/HomePage.js
+++ demo/src/components/Home/HomePage.js
@@ -5,6 +5,16 @@ const HomePage = (props) => {
       <video autoPlay muted loop>
         <source src={videoHomePage} />
       </video>
+      <div className="homepage-content">
+        <div className="title-home-page">There's a better way to ask</div>
+        <div className="content-home-page">
+          You don't want to make a boring from. And your audience won't answer
+          one. Create a typeform instead-and make everyone happy.
+        </div>
+        <div className="button-home-page">
+          <button>Get's started. It's free</button>
+        </div>
+      </div>
     </div>
   );
 };
