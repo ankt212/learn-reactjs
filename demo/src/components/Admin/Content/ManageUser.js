6ee597614b7ef8a1af45d327dfa6f8c00cc31a9f
rogerkieu <kieuthaian0212@gmail.com>
2022-09-06 16:57:08 +0700

    design modal update user

--- demo/src/components/Admin/Content/ManageUser.js
+++ demo/src/components/Admin/Content/ManageUser.js
@@ -4,21 +4,35 @@ import { FcPlus } from "react-icons/fc";
 import TableUser from "./TableUser";
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../../../services/apiServices";
+import ModalUpdateUser from "./ModalUpdateUser";
 const ManageUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
-  const handleShowHideModal = (value) => {
+  const [showModalUpdateUser, setShowModalUpdateUser] = useState(false);
+  const [dataUpdate, setDataUpdate] = useState({});
+
+  const handleShowHideModalCreateUser = (value) => {
     setShowModalCreateUser(value);
+  };
+
+  const handleShowHideModalUpdateUser = (value) => {
+    setShowModalUpdateUser(value);
   };
 
   const [listUsers, setListUsers] = useState([]);
   useEffect(() => {
     fetchListUsers();
   }, []);
+
   const fetchListUsers = async () => {
     let response = await getAllUsers();
     if (response.EC === 0) {
       setListUsers(response.DT);
     }
+  };
+
+  const handleClickBtnUpdate = (user) => {
+    setShowModalUpdateUser(true);
+    setDataUpdate(user);
   };
   return (
     <div className="manage-user-container">
@@ -33,13 +47,21 @@ const ManageUser = (props) => {
           </button>
         </div>
         <div className="table-users-container">
-          <TableUser listUsers={listUsers} />
+          <TableUser
+            listUsers={listUsers}
+            handleClickBtnUpdate={handleClickBtnUpdate}
+          />
         </div>
         <ModalCreateUser
           show={showModalCreateUser}
           // setShow={setShowModalCreateUser}
-          setShow={handleShowHideModal}
+          setShow={handleShowHideModalCreateUser}
           fetchListUsers={fetchListUsers}
+        />
+        <ModalUpdateUser
+          show={showModalUpdateUser}
+          setShow={setShowModalUpdateUser}
+          dataUpdate={dataUpdate}
         />
       </div>
     </div>
