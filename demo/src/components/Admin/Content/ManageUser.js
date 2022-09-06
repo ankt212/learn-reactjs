09dde583d1ad1edf75cf29ee5c3e99439d7fc88d
rogerkieu <kieuthaian0212@gmail.com>
2022-09-06 09:48:15 +0700

    get All user

--- demo/src/components/Admin/Content/ManageUser.js
+++ demo/src/components/Admin/Content/ManageUser.js
@@ -1,6 +1,7 @@
 import ModalCreateUser from "./ModalCreateUser";
 import { FcPlus } from "react-icons/fc";
 import { useState } from "react";
+import TableUser from "./TableUser";
 const ManageUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
   const handleShowHideModal = (value) => {
@@ -18,7 +19,9 @@ const ManageUser = (props) => {
             <FcPlus /> Add new users
           </button>
         </div>
-        <div className="table-users-container">table users</div>
+        <div className="table-users-container">
+          <TableUser />
+        </div>
         <ModalCreateUser
           show={showModalCreateUser}
           // setShow={setShowModalCreateUser}
