12f99204b15485cec97f86877039753a8b61aa1a
rogerkieu <kieuthaian0212@gmail.com>
2022-10-19 14:37:52 +0700

    api dashboard

--- demo/src/services/apiServices.js
+++ demo/src/services/apiServices.js
@@ -135,6 +135,9 @@ const logOut = (email, refresh_token) => {
   return axios.post(`api/v1/logout`, { email, refresh_token });
 };
 
+const getOverview = () => {
+  return axios.get(`api/v1/overview`);
+};
 export {
   postCreateNewUser,
   getAllUsers,
@@ -156,4 +159,5 @@ export {
   getQuizWithQA,
   postUpsertQA,
   logOut,
+  getOverview,
 };
